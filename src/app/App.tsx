8505df61f6454dbedc59d4cc62f01b2a3dbafd48
Jakub Fałowski <kubafa99@gmail.com>
2025-08-25 10:58:23 +0200

    feat(BA-3): Configure the Tailwind CSS theme with brand and accent colors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="p-16">
-      <h1 className="text-2xl font-bold">Hello ParkApp 👋</h1>
+      <h1 className="text-2xl font-bold bg-dark-yellow text-dark-blue">Hello ParkApp 👋</h1>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import tailwindConfig from "./tailwind.config.js"
+
 export default defineConfig({
   base: "/ParkApp/", 
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss({ config: tailwindConfig })],
 });
